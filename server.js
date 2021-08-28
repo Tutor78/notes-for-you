fdc349ebb84234afd262849a9371d155d3057b36
Tutor <tutor.michael78@gmail.com>
2021-08-28 08:26:16 -0500

    added get route for the notes

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const { notes } = require('./db/db.json');
+
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+
+    res.json(results);
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
