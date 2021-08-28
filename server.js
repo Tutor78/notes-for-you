c1807da55b56952b5fe80cff1386849a8df57cc7
Tutor <tutor.michael78@gmail.com>
2021-08-28 16:05:41 -0500

    added post route to save notes with a unique id

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { notes } = require('./db/db.json');
+
+function createNewNote(body, notesArray) {
+    const note = body;
+
+    notesArray.push(note);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+};
 
 app.use(express.static('public'));
 
@@ -16,6 +32,14 @@ app.get('/api/notes', (req, res) => {
     let results = notes;
 
     res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
 });
 
 app.get('/', (req, res) => {
