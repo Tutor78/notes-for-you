6ebf537c7c740e9844ab9daea2c2ea2faad13f71
Michael Tutor <tutor.michael78@gmail.com>
2021-08-28 20:14:36 -0500

    fixed an issue with the routes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,17 +3,17 @@ const router = require('express').Router();
 
 // serves the index file when the user gets to the url
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 // serves the notes file when the user affixes /notes to the url
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 // serves the index file when any other path is used in the url
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
